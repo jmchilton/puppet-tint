378f2e0ba842d9cf124894cc5ab757ba0fbbf5c1
John Chilton <jmchilton@gmail.com>
2013-02-04 13:18:17 -0600

    Bug fixes.

--- spec/classes/tint_analytics_spec.rb
+++ spec/classes/tint_analytics_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe 'tint::analytics' do
-  let(:params) { { 'version' => '1.19.18' } }
 
-  it { should contain_tomcat__deployment('rawextract').with_path('http://artifactory.msi.umn.edu/ext-release-local/msi/umn/edu/rawextract/1.19.18/rawextract-1.19.18.war') }
+  describe 'specifing version' do
+    
+    let(:params) { { 'version' => '1.19.19' } }
+    
+    it { should contain_tomcat__deployment('rawextract').with_path('http://artifactory.msi.umn.edu/ext-release-local/msi/umn/edu/rawextract/1.19.19/rawextract-1.19.19.war') }
+    
+  end
+
+
 
 end
